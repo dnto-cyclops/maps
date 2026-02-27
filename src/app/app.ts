28d8f0a2e9f0a14fcc9b149ef615e4bf5cad1efc
Jose Daniel Hernandez Cano <j.hernandez@cyclopslab.com>
2026-02-26 20:02:32 -0500

    feat(app): inicializar configuración global, estilos y bootstrap del app

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './layout/sidebar.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, SidebarComponent],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {}
